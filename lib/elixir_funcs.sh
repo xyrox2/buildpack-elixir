616af16a9b557edcd2ed37543a8c8fe74b738a92
Roman Lewin <xyrox2@Jin-Ocelott.local>
2016-08-19 18:16:15 +0200

    update readme

---
+++ lib/elixir_funcs.sh
@@ -0,0 +1,100 @@
+function download_elixir() {
+  fix_elixir_version
+
+  # If a previous download does not exist, then always re-download
+  if [ ${force_fetch} = true ] || [ ! -f ${cache_path}/$(elixir_download_file) ]; then
+    clean_elixir_downloads
+    elixir_changed=true
+
+    output_section "Fetching Elixir ${elixir_version}"
+
+    local download_url="https://s3.amazonaws.com/s3.hex.pm/builds/elixir/${elixir_version}.zip"
+    curl -s ${download_url} -o ${cache_path}/$(elixir_download_file) || exit 1
+  else
+    output_section "Using cached Elixir ${elixir_version}"
+  fi
+}
+
+function install_elixir() {
+  output_section "Installing Elixir ${elixir_version} $(elixir_changed)"
+
+  mkdir -p $(elixir_path)
+  cd $(elixir_path)
+
+  if type "unzip" &> /dev/null; then
+    unzip -q ${cache_path}/$(elixir_download_file)
+  else
+    jar xf ${cache_path}/$(elixir_download_file)
+  fi
+
+  cd - > /dev/null
+
+  chmod +x $(elixir_path)/bin/*
+  PATH=$(elixir_path)/bin:${PATH}
+
+  export LC_CTYPE=en_US.utf8
+}
+
+function fix_elixir_version() {
+  if [ ${#elixir_version[@]} -eq 2 ] && [ ${elixir_version[0]} = "branch" ]; then
+    force_fetch=true
+    elixir_version=${elixir_version[1]}
+
+  elif [ ${#elixir_version[@]} -eq 1 ]; then
+    force_fetch=false
+
+    # If we detect a version string (ex: 0.15.1) we prefix it with "v"
+    if [[ ${elixir_version} =~ ^[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+      elixir_version=v${elixir_version}
+    fi
+
+  else
+    output_line "Invalid Elixir version specified"
+    output_line "See the README for allowed formats at:"
+    output_line "https://github.com/HashNuke/heroku-buildpack-elixir"
+    exit 1
+  fi
+}
+
+function elixir_download_file() {
+  echo elixir-${elixir_version}.zip
+}
+
+function clean_elixir_downloads() {
+  rm -rf ${cache_path}/elixir*.zip
+}
+
+function restore_mix() {
+  if [ -d $(mix_backup_path) ]; then
+    cp -pR $(mix_backup_path) ${HOME}/.mix
+  fi
+
+  if [ -d $(hex_backup_path) ]; then
+    cp -pR $(hex_backup_path) ${HOME}/.hex
+  fi
+}
+
+function backup_mix() {
+  # Delete the previous backups
+  rm -rf $(mix_backup_path) $(hex_backup_path)
+
+  cp -pR ${HOME}/.mix $(mix_backup_path)
+  cp -pR ${HOME}/.hex $(hex_backup_path)
+}
+
+function install_hex() {
+  output_section "Installing Hex"
+  mix local.hex --force
+}
+
+function install_rebar() {
+  output_section "Installing rebar"
+
+  mix local.rebar --force
+}
+
+function elixir_changed() {
+  if [ $elixir_changed = true ]; then
+    echo "(changed)"
+  fi
+}
---
+++ lib/misc_funcs.sh
@@ -0,0 +1,90 @@
+# Outputs log line
+#
+# Usage:
+#
+#     output_line "Cloning repository"
+#
+function output_line() {
+  local spacing="      "
+  echo "${spacing} $1"
+}
+
+# Outputs section heading
+#
+# Usage:
+#
+#     output_section "Application tasks"
+#
+function output_section() {
+  local indentation="----->"
+  echo "${indentation} $1"
+}
+
+
+function load_config() {
+  output_section "Checking Erlang and Elixir versions"
+
+  local custom_config_file="${build_path}/elixir_buildpack.config"
+
+  # Source for default versions file from buildpack first
+  source "${build_pack_path}/elixir_buildpack.config"
+
+  if [ -f $custom_config_file ];
+  then
+    source $custom_config_file
+  else
+    output_line "WARNING: elixir_buildpack.config wasn't found in the app"
+    output_line "Using default config from Elixir buildpack"
+  fi
+
+  output_line "Will use the following versions:"
+  output_line "* Stack ${STACK}"
+  output_line "* Erlang ${erlang_version}"
+  output_line "* Elixir ${elixir_version[0]} ${elixir_version[1]}"
+  output_line "Will export the following config vars:"
+  output_line "* Config vars ${config_vars_to_export[*]}"
+}
+
+
+# Make the config vars from config_vars_to_export available at slug compile time.
+# Useful for compiled languages like Erlang and Elixir
+function export_config_vars() {
+  for config_var in ${config_vars_to_export[@]}; do
+    if [ -d $env_path ] && [ -f $env_path/${config_var} ]; then
+      export ${config_var}=$(cat $env_path/${config_var})
+    fi
+  done
+}
+
+function export_mix_env() {
+  if [ -z "$MIX_ENV" ]; then 
+    if [ -d $env_path ] && [ -f $env_path/MIX_ENV ]; then
+      export MIX_ENV=$(cat $env_path/MIX_ENV)
+    else
+      export MIX_ENV=prod
+    fi
+  fi
+
+  output_line "* MIX_ENV=${MIX_ENV}"
+}
+
+function check_stack() {
+  if [ "${STACK}" = "cedar" ]; then
+    echo "ERROR: cedar stack is not supported, upgrade to cedar-14"
+    exit 1
+  fi
+
+  if [ ! -f "${cache_path}/stack" ] || [ $(cat "${cache_path}/stack") != "${STACK}" ]; then
+    output_section "Stack changed, will rebuild"
+    rm -rf ${cache_path}/*
+  fi
+
+  echo "${STACK}" > "${cache_path}/stack"
+}
+
+function clean_cache() {
+  if [ $always_rebuild = true ]; then
+    output_section "Cleaning all cache to force rebuilds"
+    rm -rf $cache_path/*
+  fi
+}
